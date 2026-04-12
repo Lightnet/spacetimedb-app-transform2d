6eb779071d09f9082398440614cba7a3b3ec4389
lightnet <darkneter@gmail.com>
2026-04-11 20:47:10 -0700

    added missing.

---
+++ src/context.js
@@ -0,0 +1,7 @@
+// context
+
+import van from "vanjs-core";
+
+export const userIdentity = van.state(null);
+export const networkStatus = van.state('Not Connected!');
+export const connState = van.state(null);
